efb09d541882c590473e7c0c67d7bb10f84925a3
yama-chiiii <nagayama0409@gmail.com>
2023-04-23 04:40:42 +0900

    細かいところの修正

--- src/pages/result.tsx
+++ src/pages/result.tsx
@@ -4,18 +4,18 @@ import HistoryList from './../componemts/historyList'
 const Result = () => {
   return (
     <div className="flex w-full min-h-screen">
-      {/* サイドバーの実装 */}
       <div className="w-1/4 bg-brown-dark">
         <HistoryList />
       </div>
-      {/* 要約文を受け取るところ */}
       <div className="flex flex-col mt-10 mx-10 w-3/4 h-screen">
-        <div className="bg-gray-light w-2/4 h-60">{/* 質問が入る */}</div>
+        <h1 className="font-black">質問</h1>
+        <div className="bg-gray-light w-2/4 h-60">{/* <input></input> */}</div>
         <div className="flex justify-end">
           <div className="mt-10 text-right bg-gray-light w-2/4 h-60">
-            {/* 要約文 */}
+            {/* <input></input> */}
           </div>
         </div>
+        <h1 className="font-black text-right">要約文</h1>
       </div>
     </div>
   )
