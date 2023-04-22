c22ad7bfbad6e2992ee7660c18bba097d8ac7a75
kototo522 <huyejiben@gmail.com>
2023-04-22 18:01:58 +0900

    add: feature/issue#04

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,5 +1,8 @@
 import { useRouter } from 'next/router'
 import { useState } from 'react'
+import { app } from '../Firebase/firebase'
+
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth'
 
 type changeEvent = {
   target: {
@@ -8,14 +11,18 @@ type changeEvent = {
 }
 
 const Login = () => {
+  const auth = getAuth(app)
   const router = useRouter()
   const [name, setName] = useState('')
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
 
-  function jumpHome() {
-    name && email && password ? router.push('./') : console.log('未入力')
-    console.log(name, email, password)
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    if (name && email && password) {
+      await signInWithEmailAndPassword(auth, email, password)
+      router.push('./')
+    }
   }
 
   const handleNameChange = (event: changeEvent) => {
@@ -31,35 +38,36 @@ const Login = () => {
   return (
     <div className="w-full h-screen flex flex-col justify-center items-center">
       <h2 className="text-6xl font-black">Login</h2>
-      <div className="flex flex-col mt-10 w-1/3">
+      <form onSubmit={handleSubmit}>
+        <div className="flex flex-col mt-10 w-1/3">
+          <input
+            className="border"
+            type="text"
+            id="name"
+            name="name"
+            onChange={handleNameChange}
+          />
+          <input
+            className="mt-2 border"
+            type="email"
+            id="email"
+            name="email"
+            onChange={handleEmailChange}
+          />
+          <input
+            className="mt-2 border"
+            type="password"
+            id="password"
+            name="password"
+            onChange={handlePasswordChange}
+          />
+        </div>
         <input
-          className="border"
-          type="text"
-          id="name"
-          name="name"
-          onChange={handleNameChange}
+          className="mt-10 px-10 font-black border"
+          type="submit"
+          value="start"
         />
-        <input
-          className="mt-2 border"
-          type="email"
-          id="email"
-          name="email"
-          onChange={handleEmailChange}
-        />
-        <input
-          className="mt-2 border"
-          type="password"
-          id="password"
-          name="password"
-          onChange={handlePasswordChange}
-        />
-      </div>
-      <input
-        className="mt-10 px-10 font-black border"
-        type="submit"
-        value="start"
-        onClick={jumpHome}
-      />
+      </form>
     </div>
   )
 }
