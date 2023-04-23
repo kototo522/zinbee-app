df50805331ec90a06d338db077c997c800ad9216
yama-chiiii <nagayama0409@gmail.com>
2023-04-23 12:59:59 +0900

    --noedit

--- src/componemts/record.tsx
+++ src/componemts/record.tsx
@@ -4,6 +4,19 @@ import { memo } from 'react'
 import SpeechRecognition, {
   useSpeechRecognition,
 } from 'react-speech-recognition'
+const Record = () => {
+  return (
+    <div>
+      <div className="flex flex-col justify-center items-center w-full h-screen">
+        <h1 className="mb-5 font-black text-xl">音声を入力</h1>
+        <input type="image" src="mike.svg" alt="送信" />
+      </div>
+      <h2 className="flex flex-row-reverse items-end font-black text-lg">
+        ログアウト
+      </h2>
+    </div>
+  )
+}
 
 const Record = memo(
   ({ messages, setMessages }: GptMessagesProps): JSX.Element => {
