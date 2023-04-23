159d3f34ca5aa2054f11c7e0f10290022d37e623
kototo522 <huyejiben@gmail.com>
2023-04-23 15:29:40 +0900

    やったらダメなこと2回目

---
+++ src/interface/interface.ts
@@ -0,0 +1,6 @@
+export type SampleType = {
+  // userId: string;
+  userName: string
+  userEmail: string
+  userPassword: string
+}
--- src/pages/signUp.tsx
+++ src/pages/signUp.tsx
@@ -1,7 +1,7 @@
 import { useRouter } from 'next/router'
 import { useState } from 'react'
 import { app } from '../Firebase/firebase'
-import { getAuth, signInWithEmailAndPassword } from 'firebase/auth'
+import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth'
 import toast, { Toaster } from 'react-hot-toast'
 import Link from 'next/link'
 
@@ -11,47 +11,25 @@ type changeEvent = {
   }
 }
 
-const Login = () => {
+const SignUp = () => {
   const auth = getAuth(app)
   const router = useRouter()
+  const [name, setName] = useState('')
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
-    await signInWithEmailAndPassword(auth, email, password)
-      .then(() => {
-        toast.success(`Welcome back！`)
-        signInWithEmailAndPassword(auth, email, password)
-        router.push('./')
-      })
-      .catch((error) => {
-        handleLoginError(error)
-      })
+    if (name && email && password) {
+      toast.success('finish created user!')
+      createUserWithEmailAndPassword(auth, email, password)
+      router.push('./')
+    } else toast.error('入力に不備があります')
   }
 
-  function handleLoginError(error: { code: any }) {
-    switch (error.code) {
-      case 'auth/wrong-password':
-        toast.error('パスワードが間違っています。')
-        break
-      case 'auth/user-not-found':
-        toast.error('ユーザーが見つかりません。')
-        break
-      case 'auth/invalid-email':
-        toast.error('正しい形式のメールアドレスを入力してください。')
-        break
-      case 'auth/too-many-requests':
-        toast.error(
-          'アカウントが一時的にロックされています。しばらくしてから再度お試しください。'
-        )
-        break
-      default:
-        toast.error('ログインに失敗しました。')
-        break
-    }
+  const handleNameChange = (event: changeEvent) => {
+    setName(event.target.value)
   }
-
   const handleEmailChange = (event: changeEvent) => {
     setEmail(event.target.value)
   }
@@ -61,12 +39,19 @@ const Login = () => {
 
   return (
     <div className="w-full h-screen flex flex-col justify-center items-center bg-gradient-to-t from-gray-light to-white">
-      <h2 className="text-6xl font-black">Login</h2>
+      <h2 className="text-6xl font-black">SignUp</h2>
       <form
         onSubmit={handleSubmit}
         className="w-full flex flex-col justify-center items-center"
       >
         <div className="flex flex-col mt-10 w-1/3">
+          <input
+            className="mt-2 border"
+            type="name"
+            id="name"
+            name="name"
+            onChange={handleNameChange}
+          />
           <input
             className="mt-2 border"
             type="email"
@@ -90,12 +75,12 @@ const Login = () => {
         <Toaster />
       </form>
       <div className=" flex flex-col items-center justify-center ">
-        <Link className="border-b mt-10" href="/signUp">
-          新規登録はこちら
+        <Link className="border-b mt-10" href="/login">
+          ログインはこちら
         </Link>
       </div>
     </div>
   )
 }
 
-export default Login
+export default SignUp
